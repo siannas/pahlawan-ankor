05cb94138e87b21a995e66d1de879846b38153d5
Unknown <massive.annas@gmail.com>
2019-11-10 22:19:04 +0700

    added Scenario

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using DG.Tweening;
+using KoganeUnityLib;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject person1;
+    public GameObject person2;
+    public GameObject dialogBox;
+    
+    GameObject dialogMaster;
+    TextMeshProUGUI dialogLine;
+
+    public TMP_Typewriter m_typewriter;
+    public float m_speed;
+
+    bool complete = true;
+
+    private dialogueTemplate dt;
+
+    void Start()
+    {
+        dialogMaster = gameObject;
+        dialogLine = dialogBox.GetComponentInChildren<TextMeshProUGUI>();
+        dt = gameObject.GetComponent<dialogueTemplate>();
+        dt.startScenarioAt(0);
+        continueLine();
+    }
+
+    private void showDialogLine(string line)
+    {
+        complete = false;
+        m_typewriter.Play
+        (
+            text: line,
+            speed: m_speed,
+            onComplete: () => complete=true
+        );
+    }
+
+    public void continueLine()
+    {
+        if (!complete){
+            m_typewriter.Skip();
+        }
+        else
+        {
+            string line = dt.getNextLine();
+            showDialogLine(line);
+        }
+    }
+
+    private void showHide(string thing, bool isShow)
+    {
+        switch (thing)
+        {
+            case "person1":
+                person1.SetActive(isShow);
+                break;
+            case "person2":
+                person2.SetActive(isShow);
+                break;
+            default:
+                dialogMaster.SetActive(isShow);
+                dialogLine.SetText("");
+                person1.SetActive(true);
+                person2.SetActive(true);
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            continueLine();
+        }
+    }
+}
---
+++ Assets/Scripts/Dialogue/dialogueTemplate.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+namespace KoganeUnityLib
+{
+    public class Dialog
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public Dictionary<string, string> persons { get; set; }
+        public List<Dictionary<string, string>> dialogue { get; set; }
+    }
+
+    public class dialogueTemplate : MonoBehaviour
+    {
+        const String dialoguePath = "Assets/Files/dialogue.json";
+        string dialogue;
+
+        private IList<Dialog> scenario;
+
+        private Dictionary<string, string> persons;
+
+        private List<Dictionary<string, string>>.Enumerator currScenario;
+
+        private bool allComplete = false;
+
+
+        void Start()
+        {
+            StreamReader stream = new StreamReader(dialoguePath);
+            dialogue = stream.ReadToEnd();
+            scenario = JsonConvert.DeserializeObject<IList<Dialog>>(dialogue);
+        }
+
+
+
+        public void startScenarioAt(int index)
+        {
+            persons = scenario[index].persons;
+            currScenario = scenario[index].dialogue.GetEnumerator();
+        }
+
+        public string getNextLine()
+        {
+            try
+            {
+                currScenario.MoveNext();
+                foreach (KeyValuePair<string, string> line in currScenario.Current)
+                {
+                    return line.Value;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+
+}
